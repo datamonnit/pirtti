407e1f5a14dae0cec495b4e454fddbd8fd095164
Juho Pirilä <juho.pirila@datanomit.local>
2017-04-05 14:08:16 +0300

    kaskd

--- paivakotipirtti/send.php
+++ paivakotipirtti/send.php
@@ -3,20 +3,36 @@
 	$lapsietunimi = $_POST['lapsietunimi'];
 	$lapsisukunimi = $_POST['lapsisukunimi'];
 	$lapsihenkilotunnus = $_POST['lapsihenkilotunnus'];
+	$lapsiaidinkieli = $_POST['lapsiaidinkieli'];
+	$lemmikkeja = $_POST['lemmikkeja'];
+	$huoltajaetunimi = $_POST['huoltajaetunimi'];
+	$huoltajasukunimi = $_POST['huoltajasukunimi'];
+	$huoltajahenkilotunnus = $_POST['huoltajahenkilotunnus']
 	$message =
  "Lapsen etunimet: $lapsietunimi $sukunimi "
-	.PHP_EOL.	"Lapsen sukunimi $lapsisukunimi"
-	.PHP_EOL. "Lapsen henkilötunnus $lapsihenkilotunnus";
+	.PHP_EOL.	"Lapsen sukunimi: $lapsisukunimi"
+	.PHP_EOL. "Lapsen henkilötunnus: $lapsihenkilotunnus"
+	.PHP_EOL. "Lapsen äidinkieli: $lapsiaidinkieli"
+	.PHP_EOL. "Onko kotona lemmikkieläimiä: $lemmikkeja"
+	.PHP_EOL. PHP_EOL. PHP_EOL. PHP_EOL. PHP_EOL.
+	PHP_EOL. "Huoltajan etunimi: $huoltajaetunimi"
+	.PHP_EOL. "Huoltajan sukunimi: $huoltajasukunimi";
+	.PHP_EOL. "Huoltajan sukunimi: $huoltajahenkilotunnus";
+
+
+
 
 	if($lapsietunimi==''){
 	echo "<script>alert('Some of the fields are empty')</script>";
 	exit();
 	}else{
-		$headers = 'From: paivahoitohakemus@pirtti.com' . "\r\n" .
-						    'Reply-To: webmaster@example.com' . "\r\n" .
+		$headers = 'From: noreply@pirtti.com' . "\r\n" .
+						    'Reply-To: oreply@pirtti.com' . "\r\n" .
 						    'X-Mailer: PHP/' . phpversion();
-	mail('juho.pirila@outlook.com', $lapsietunimi,$message, $headers);
-	echo "<center><h1>Feedback sent successfully!</h1></center>";
+	mail('juho.pirila@outlook.com', 'Paivahoitohakemus',$message, $headers);
+
+
+	echo "<center><h1>Päivähoitohakemus lähetetty!</h1></center>";
 
 
 }
