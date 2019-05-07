2ebb0ff38abb51f2bfe86ec04119a0208aed4604
Tuomas Puro <tuomas.puro@itvelhot.local>
2019-05-07 09:12:47 +0300

    SQL-dumppi lisätty

---
+++ db/pirtti-db.sql
@@ -0,0 +1,132 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pirttidb
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.37-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `esiopetushakemus`
+--
+
+DROP TABLE IF EXISTS `esiopetushakemus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `esiopetushakemus` (
+  `huoltajasahkoposti` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `huoltajapuhelin` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `pvm` varchar(50) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `hash` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `paivakoti` varchar(50) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `esiopetushakemus`
+--
+
+LOCK TABLES `esiopetushakemus` WRITE;
+/*!40000 ALTER TABLE `esiopetushakemus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `esiopetushakemus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gallery`
+--
+
+DROP TABLE IF EXISTS `gallery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gallery` (
+  `gallery_id` int(11) NOT NULL AUTO_INCREMENT,
+  `otsikko` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `time` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `image_text` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `img_path` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `img_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `img_type` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `img_path_thumb` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`gallery_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery`
+--
+
+LOCK TABLES `gallery` WRITE;
+/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gallery_user`
+--
+
+DROP TABLE IF EXISTS `gallery_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gallery_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8_swedish_ci NOT NULL,
+  `password` varchar(45) COLLATE utf8_swedish_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery_user`
+--
+
+LOCK TABLES `gallery_user` WRITE;
+/*!40000 ALTER TABLE `gallery_user` DISABLE KEYS */;
+INSERT INTO `gallery_user` VALUES (6,'miklas','7ec613c5ad719ea864ab5eb8a5fb3cd6','maczulskijmiklas@gmail.com'),(7,'tuomas','d8578edf8458ce06fbc5bb76a58c5ca4','tuomas.puro@esedu.fi');
+/*!40000 ALTER TABLE `gallery_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paivahoitohakemus`
+--
+
+DROP TABLE IF EXISTS `paivahoitohakemus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paivahoitohakemus` (
+  `huoltajasahkoposti` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `huoltajapuhelin` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
+  `pvm` date NOT NULL,
+  `hash` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `paivakoti` varchar(20) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paivahoitohakemus`
+--
+
+LOCK TABLES `paivahoitohakemus` WRITE;
+/*!40000 ALTER TABLE `paivahoitohakemus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paivahoitohakemus` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-06 13:43:36
