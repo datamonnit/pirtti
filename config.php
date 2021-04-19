15b79736649d62b622337cf05d870b8f0da40d0c
eppuo <70254908+eppuo@users.noreply.github.com>
2021-04-19 15:05:14 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "pkpirtti_user";
-$password = "foFIKEW{Y-}8";
+$username = "root";
+$password = "qwerty321";
 $database = "pkpirtti_pirtti_db";
 
 $http_host = "http://localhost/pirti/pirtti/";
